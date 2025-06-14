dabbb6cc448c248fa4fdd72435813de513c7baa6
hanaaden <hanaadamabdi@gmail.com>
2025-06-14 09:28:26 +0300

    updated logic of navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -12,7 +12,8 @@ function Navbar() {
         axios.get('https://blog-app-c5fz.onrender.com/logout', { withCredentials: true })
             .then(res => {
                 if (res.data === "success") {
-                    setUser({});
+                    // setUser({})
+                    setUser(null);
                     navigate('/');
                 }
             })
@@ -24,10 +25,10 @@ function Navbar() {
             <h3>Blog App</h3>
             <div className="navbar-links">
                 <Link to='/' className='link'>Home</Link>
-                {user.username && <Link to='/create' className='link'>Create</Link>}
+                {user?.username && <Link to='/create' className='link'>Create</Link>}
                 <Link to="/contactus" className='link'>Contact Us</Link>
             </div>
-            {user.username ? (
+            {user?.username ? (
                 <div>
                     <input type="button" value="Logout" onClick={handleLogout} className='btn_input' />
                 </div>
