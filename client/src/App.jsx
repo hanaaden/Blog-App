7db0433924b2f193005f200aad7638fcfbe957f5
hanaaden <hanaadamabdi@gmail.com>
2025-06-14 09:38:37 +0300

    updated logic of navbar in context and both app page and nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,69 @@
+// import React, { createContext, useEffect, useState } from 'react';
+// import Navbar from './Navbar';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Register from './Register';
+// import Login from './login';
+// import Home from './Home';
+// import CreatePost from './CreatePost';
+// import Post from './Post';
+// import EditPost from './EditPost';
+// import ContactUs from './ContactUs'; // Import the ContactUs component
+// import axios from 'axios';
+// import HamburgerMenu from './HamburgerMenu';
+
+// // https://blog-app-c5fz.onrender.com
+
+// export const UserContext = createContext();
+
+// function App() {
+//     // const [user, setUser] = useState({});
+//     const [user, setUser] = useState(null);
+
+//     axios.defaults.withCredentials = true;
+
+//     useEffect(() => {
+//         axios.get('https://blog-app-c5fz.onrender.com/', { withCredentials: true })
+//             .then(response => {
+//                 setUser(response.data);
+//             })
+//             .catch(err => console.log(err));
+//     }, []);
+
+//     const handleLogout = () => {
+//         axios.get('https://blog-app-c5fz.onrender.com/logout', { withCredentials: true })
+//             .then(res => {
+//                 if (res.data === "success") {
+//                     setUser({});
+//                 }
+//             })
+//             .catch(err => console.log(err));
+//     };
+
+//     return (
+//         <UserContext.Provider value={{ user, setUser }}>
+//             <BrowserRouter>
+//                 <Navbar />
+//                 <HamburgerMenu user={user} onLogout={handleLogout} />
+//                 <Routes>
+//                     <Route path="/" element={<Home />} />
+//                     <Route path="/register" element={<Register />} />
+//                     <Route path="/login" element={<Login />} />
+//                     <Route path="/create" element={<CreatePost />} />
+//                     <Route path="/post/:id" element={<Post />} />
+//                     <Route path="/editpost/:id" element={<EditPost />} />
+//                     <Route path="/contactus" element={<ContactUs />} /> {/* Add this route */}
+//                 </Routes>
+//             </BrowserRouter>
+//         </UserContext.Provider>
+//     );
+// }
+
+// export default App;
+
+
+
+
+
 import React, { createContext, useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -7,17 +73,15 @@ import Home from './Home';
 import CreatePost from './CreatePost';
 import Post from './Post';
 import EditPost from './EditPost';
-import ContactUs from './ContactUs'; // Import the ContactUs component
+import ContactUs from './ContactUs';
 import axios from 'axios';
 import HamburgerMenu from './HamburgerMenu';
-
-// https://blog-app-c5fz.onrender.com
 
 export const UserContext = createContext();
 
 function App() {
-    // const [user, setUser] = useState({});
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     axios.defaults.withCredentials = true;
 
@@ -26,18 +90,26 @@ function App() {
             .then(response => {
                 setUser(response.data);
             })
-            .catch(err => console.log(err));
+            .catch(err => {
+                console.log(err);
+                setUser(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }, []);
 
     const handleLogout = () => {
         axios.get('https://blog-app-c5fz.onrender.com/logout', { withCredentials: true })
             .then(res => {
                 if (res.data === "success") {
-                    setUser({});
+                    setUser(null);
                 }
             })
             .catch(err => console.log(err));
     };
+
+    if (loading) return <div>Loading...</div>; // You can replace this with a loader spinner if you prefer
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
@@ -51,7 +123,7 @@ function App() {
                     <Route path="/create" element={<CreatePost />} />
                     <Route path="/post/:id" element={<Post />} />
                     <Route path="/editpost/:id" element={<EditPost />} />
-                    <Route path="/contactus" element={<ContactUs />} /> {/* Add this route */}
+                    <Route path="/contactus" element={<ContactUs />} />
                 </Routes>
             </BrowserRouter>
         </UserContext.Provider>
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,3 +1,52 @@
+// import React, { useContext } from 'react';
+// import './style.css';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { UserContext } from './App';
+// import axios from 'axios';
+
+// function Navbar() {
+//     const { user, setUser } = useContext(UserContext);
+//     const navigate = useNavigate();
+
+//     const handleLogout = () => {
+//         axios.get('https://blog-app-c5fz.onrender.com/logout', { withCredentials: true })
+//             .then(res => {
+//                 if (res.data === "success") {
+//                     // setUser({})
+//                     setUser(null);
+//                     navigate('/');
+//                 }
+//             })
+//             .catch(err => console.log(err));
+//     };
+
+//     return (
+//         <div className='navbar-header'>
+//             <h3>Blog App</h3>
+//             <div className="navbar-links">
+//                 <Link to='/' className='link'>Home</Link>
+//                 {user?.username && <Link to='/create' className='link'>Create</Link>}
+//                 <Link to="/contactus" className='link'>Contact Us</Link>
+//             </div>
+//             {user?.username ? (
+//                 <div>
+//                     <input type="button" value="Logout" onClick={handleLogout} className='btn_input' />
+//                 </div>
+//             ) : (
+//                 <h5><Link to="/register" className='link'>Register/Login</Link></h5>
+//             )}
+//         </div>
+//     );
+// }
+
+// export default Navbar;
+
+
+
+
+
+
+
 import React, { useContext } from 'react';
 import './style.css';
 import { Link, useNavigate } from 'react-router-dom';
@@ -12,7 +61,6 @@ function Navbar() {
         axios.get('https://blog-app-c5fz.onrender.com/logout', { withCredentials: true })
             .then(res => {
                 if (res.data === "success") {
-                    // setUser({})
                     setUser(null);
                     navigate('/');
                 }
@@ -25,10 +73,10 @@ function Navbar() {
             <h3>Blog App</h3>
             <div className="navbar-links">
                 <Link to='/' className='link'>Home</Link>
-                {user?.username && <Link to='/create' className='link'>Create</Link>}
+                {user && user.username && <Link to='/create' className='link'>Create</Link>}
                 <Link to="/contactus" className='link'>Contact Us</Link>
             </div>
-            {user?.username ? (
+            {user && user.username ? (
                 <div>
                     <input type="button" value="Logout" onClick={handleLogout} className='btn_input' />
                 </div>
